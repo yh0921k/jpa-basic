ec282357a720ba7bbc064fd669558f5ac201b6b1
Yonghwi Kim <yh0921k@gmail.com>
2022-10-17 20:50:21 +0900

    feat: JPA를 이용한 사용자 등록

--- src/main/java/study/jpa_basic/JpaMain.java
+++ src/main/java/study/jpa_basic/JpaMain.java
@@ -2,6 +2,7 @@ package study.jpa_basic;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,8 +11,22 @@ public class JpaMain {
 
     EntityManager em = emf.createEntityManager();
 
-    em.close();
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
 
+    try {
+      Member member = new Member();
+      member.setId(2L);
+      member.setName("HelloB");
+
+      em.persist(member);
+
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
     emf.close();
   }
 }
