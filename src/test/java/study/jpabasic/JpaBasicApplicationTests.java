1108d7b7214661091ed42c64017ed402bb7c7459
Yonghwi Kim <yh0921k@gmail.com>
2022-10-17 17:56:33 +0900

    init: JPA 학습을 위한 저장소 초기화
    .gitignore
    - 일부 키워드로 추적 제외 명시
    
    build.gradle
    - H2 Database 의존성 버전 변경
    
    application.yml
    - 데이터베이스 관련 설정 추가

---
+++ src/test/java/study/jpabasic/JpaBasicApplicationTests.java
@@ -0,0 +1,11 @@
+package study.jpabasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
