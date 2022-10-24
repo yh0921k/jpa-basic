e5aa0d156ab18db070251be2cfe752d56a239a8d
Yonghwi Kim <yh0921k@gmail.com>
2022-10-24 09:24:58 +0900

    feat: Section 06. 일대다 양방향

--- src/main/java/study/jpa_basic/Member.java
+++ src/main/java/study/jpa_basic/Member.java
@@ -14,6 +14,10 @@ public class Member {
   @Column(name = "USERNAME")
   private String username;
 
+  @ManyToOne
+  @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+  private Team team;
+
   public Long getId() {
     return id;
   }
