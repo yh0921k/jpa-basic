12f7c48df725b7f0b26e78da077946b4230a992f
Yonghwi Kim <yh0921k@gmail.com>
2022-10-25 11:45:26 +0900

    feat: Section 08. 프록시의 특징(비교)

--- src/main/java/study/jpa_basic/JpaMain.java
+++ src/main/java/study/jpa_basic/JpaMain.java
@@ -16,19 +16,26 @@ public class JpaMain {
 
     try {
 
-      Member member = new Member();
-      member.setUsername("AAA");
-      em.persist(member);
+      Member member1 = new Member();
+      member1.setUsername("AAA");
+      em.persist(member1);
+
+      Member member2 = new Member();
+      member2.setUsername("BBB");
+      em.persist(member2);
+
+      Member member3 = new Member();
+      member3.setUsername("CCC");
+      em.persist(member3);
       
       em.flush();
       em.clear();
 
-//      Member findMember = em.find(Member.class, 1L);
-
-      Member findMember = em.getReference(Member.class, member.getId());
-      System.out.println("findMember = " + findMember.getClass());
-      System.out.println("findMember.getUsername() = " + findMember.getUsername());
-      System.out.println("findMember = " + findMember.getClass());
+      Member m1 = em.find(Member.class, member1.getId());
+      Member m2 = em.find(Member.class, member2.getId());
+      Member m3 = em.getReference(Member.class, member3.getId());
+      compare(m1, m2);
+      compare(m1, m3);
 
       tx.commit();
     } catch (Exception e) {
@@ -37,5 +44,10 @@ public class JpaMain {
       em.close();
     }
     emf.close();
+  }
+
+  private static void compare(Member m1, Member m2) {
+//    System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
+    System.out.println("m1 == m2 : " + (m1 instanceof Member && m2 instanceof Member));
   }
 }
