59cb4cf8a0490fa38d426b2d514afe3c101fc3a5
Yonghwi Kim <yh0921k@gmail.com>
2022-10-25 11:50:46 +0900

    feat: Section 08. 프록시의 특징(객체 동등성 보장)

--- src/main/java/study/jpa_basic/JpaMain.java
+++ src/main/java/study/jpa_basic/JpaMain.java
@@ -19,24 +19,17 @@ public class JpaMain {
       Member member1 = new Member();
       member1.setUsername("AAA");
       em.persist(member1);
-
-      Member member2 = new Member();
-      member2.setUsername("BBB");
-      em.persist(member2);
-
-      Member member3 = new Member();
-      member3.setUsername("CCC");
-      em.persist(member3);
       
       em.flush();
       em.clear();
 
       Member m1 = em.find(Member.class, member1.getId());
-      Member m2 = em.find(Member.class, member2.getId());
-      Member m3 = em.getReference(Member.class, member3.getId());
-      compare(m1, m2);
-      compare(m1, m3);
+      System.out.println("m1.getClass() = " + m1.getClass());
 
+      Member reference = em.getReference(Member.class, member1.getId());
+      System.out.println("reference.getClass() = " + reference.getClass());
+
+      System.out.println("(reference == m1) = " + (reference == m1));
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
@@ -44,10 +37,5 @@ public class JpaMain {
       em.close();
     }
     emf.close();
-  }
-
-  private static void compare(Member m1, Member m2) {
-//    System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
-    System.out.println("m1 == m2 : " + (m1 instanceof Member && m2 instanceof Member));
   }
 }
