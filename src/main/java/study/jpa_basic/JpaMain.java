e9ab3f6fcf22e10f143261a43b297fa23031f123
Yonghwi Kim <yh0921k@gmail.com>
2022-10-25 12:06:02 +0900

    feat: Section 08. 프록시 인스턴스의 초기화 여부 확인

--- src/main/java/study/jpa_basic/JpaMain.java
+++ src/main/java/study/jpa_basic/JpaMain.java
@@ -1,9 +1,6 @@
 package study.jpa_basic;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 
 public class JpaMain {
   public static void main(String[] args) {
@@ -25,10 +22,8 @@ public class JpaMain {
 
       Member refMember = em.getReference(Member.class, member1.getId());
       System.out.println("refMember.getClass() = " + refMember.getClass());
-
-      em.detach(refMember);
-
       refMember.getUsername();
+      System.out.println("emf.getPersistenceUnitUtil().isLoaded(refMember) = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
 
       tx.commit();
     } catch (Exception e) {
