c770e97dacc9a781ba6e310ff6997e521184d245
Yonghwi Kim <yh0921k@gmail.com>
2022-10-25 14:57:25 +0900

    feat: Section 08. N+1 문제

--- src/main/java/study/jpa_basic/JpaMain.java
+++ src/main/java/study/jpa_basic/JpaMain.java
@@ -3,6 +3,7 @@ package study.jpa_basic;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
   public static void main(String[] args) {
@@ -15,24 +16,29 @@ public class JpaMain {
 
     try {
 
-      Team team = new Team();
-      team.setName("TeamAAA");
-      em.persist(team);
+      Team teamA = new Team();
+      teamA.setName("TeamAAA");
+      em.persist(teamA);
+
+      Team teamB = new Team();
+      teamB.setName("TeamBBB");
+      em.persist(teamB);
 
       Member member1 = new Member();
       member1.setUsername("AAA");
-      member1.setTeam(team);
+      member1.setTeam(teamA);
       em.persist(member1);
+
+      Member member2 = new Member();
+      member2.setUsername("BBB");
+      member2.setTeam(teamB);
+      em.persist(member2);
 
       em.flush();
       em.clear();
 
-      Member m = em.find(Member.class, member1.getId());
-      System.out.println("m.getTeam().getClass() = " + m.getTeam().getClass());
-
-      System.out.println("=======================================");
-      m.getTeam().getName();
-      System.out.println("=======================================");
+//      Member m = em.find(Member.class, member1.getId());
+      List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
 
       tx.commit();
     } catch (Exception e) {
