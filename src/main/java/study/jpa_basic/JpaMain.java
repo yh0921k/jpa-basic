4b48a420be0a8de1ae12261dd6fa0d65743e3d5e
Yonghwi Kim <yh0921k@gmail.com>
2022-10-27 10:48:04 +0900

    feat: Section 09. 값타입 컬렉션 조회

--- src/main/java/study/jpa_basic/JpaMain.java
+++ src/main/java/study/jpa_basic/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
   public static void main(String[] args) {
@@ -27,6 +28,18 @@ public class JpaMain {
       member.getAddressHistory().add(new Address("city3", "street3", "zipcode3"));
 
       em.persist(member);
+
+      em.flush();
+      em.clear();
+
+      System.out.println("===================================");
+      Member findMember = em.find(Member.class, member.getId());
+      System.out.println("===================================");
+
+      List<Address> addressHistory = findMember.getAddressHistory();
+      for (Address address : addressHistory) {
+        System.out.println("address.getCity() = " + address.getCity());
+      }
 
       tx.commit();
     } catch (Exception e) {
