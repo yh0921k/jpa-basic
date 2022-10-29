32e460b538282bf84915127a898be81dc8275c28
Yonghwi Kim <yh0921k@gmail.com>
2022-10-29 16:17:24 +0900

    feat: Section 10. Criteria 소개

--- src/main/java/study/jpa_basic/BasicMain.java
+++ src/main/java/study/jpa_basic/BasicMain.java
@@ -6,6 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class BasicMain {
@@ -18,19 +21,17 @@ public class BasicMain {
     tx.begin();
 
     try {
-      Member member = new Member();
-      member.setUsername("kim yong hwi");
+      // Criteria 사용 준비
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<Member> query = cb.createQuery(Member.class);
 
-      em.persist(member);
+      // 루트 클래스 (조회를 시작할 클래스)
+      Root<Member> m = query.from(Member.class);
 
-      em.flush();
-      em.clear();
+      // 쿼리 생성
+      CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("username"), "kim"));
+      List<Member> resultList = em.createQuery(cq).getResultList();
 
-      List<Member> result =
-          em.createQuery("select m from Member m where m.username like '%kim%'", Member.class)
-              .getResultList();
-
-      System.out.println("result.get(0).getUsername() = " + result.get(0).getUsername());
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
