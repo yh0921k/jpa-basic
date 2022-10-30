b534cec24ea8959a6a6cb19f298acbe9f86e420e
Yonghwi Kim <yh0921k@gmail.com>
2022-10-30 20:40:48 +0900

    feat: Section 10. TypeQuery, Query

--- src/main/java/study/jpql/JpqlMain.java
+++ src/main/java/study/jpql/JpqlMain.java
@@ -1,9 +1,8 @@
 package study.jpql;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import study.jpql.domain.Member;
+
+import javax.persistence.*;
 
 public class JpqlMain {
   public static void main(String[] args) {
@@ -15,7 +14,14 @@ public class JpqlMain {
     tx.begin();
 
     try {
+      Member member = new Member();
+      member.setUsername("member1");
+      member.setAge(10);
+      em.persist(member);
 
+      TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+      TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
+      Query query3 = em.createQuery("select m.username, m.age from Member m");
 
       tx.commit();
     } catch (Exception e) {
