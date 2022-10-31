3369b7ebd38eee64957656391464013e59c59701
Yonghwi Kim <yh0921k@gmail.com>
2022-10-31 21:00:00 +0900

    feat: Section 10. 조건식(NULLIF)

--- src/main/java/study/jpql/JpqlMain.java
+++ src/main/java/study/jpql/JpqlMain.java
@@ -25,7 +25,7 @@ public class JpqlMain {
       em.persist(team);
 
       Member member = new Member();
-      member.setUsername(null);
+      member.setUsername("관리자");
       member.setAge(20);
       member.setTeam(team);
       member.setType(MemberType.ADMIN);
@@ -35,7 +35,7 @@ public class JpqlMain {
       em.flush();
       em.clear();
 
-      String query = "select coalesce(m.username, '이름 없는 회원') from Member m";
+      String query = "select nullif(m.username, '관리자') from Member m";
       List<String> resultList = em.createQuery(query, String.class).getResultList();
       for (String s : resultList) {
         System.out.println("s = " + s);
