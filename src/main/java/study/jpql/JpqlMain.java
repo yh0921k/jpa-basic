7c0691cc3f12d85963cccd692ab2e2a6328c166b
Yonghwi Kim <yh0921k@gmail.com>
2022-10-31 21:00:00 +0900

    feat: Section 10. 서브쿼리(SELECT)

--- src/main/java/study/jpql/JpqlMain.java
+++ src/main/java/study/jpql/JpqlMain.java
@@ -3,10 +3,7 @@ package study.jpql;
 import study.jpql.domain.Member;
 import study.jpql.domain.Team;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 
 public class JpqlMain {
@@ -32,9 +29,12 @@ public class JpqlMain {
       em.flush();
       em.clear();
 
-      String query = "select m from Member m left join Team t on m.username = t.name";
-      List<Member> resultList = em.createQuery(query, Member.class).getResultList();
+      String query = "select m.id, (select avg(m1.age) from Member m1) as avgAge from Member m left join Team t on m.username = t.name";
+      Query subQuery = em.createQuery(query);
+      List resultList = subQuery.getResultList();
       System.out.println("resultList.size() = " + resultList.size());
+      System.out.println("((Object[])resultList.get(0))[0] = " + ((Object[])resultList.get(0))[0]);
+      System.out.println("((Object[])resultList.get(0))[1] = " + ((Object[])resultList.get(0))[1]);
 
       tx.commit();
     } catch (Exception e) {
