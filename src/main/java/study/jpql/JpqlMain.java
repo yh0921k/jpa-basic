63fa64033868f4cec9a6d2689b4663347ca86935
Yonghwi Kim <yh0921k@gmail.com>
2022-10-31 21:00:00 +0900

    feat: Section 10. 기본 함수(SIZE)

--- src/main/java/study/jpql/JpqlMain.java
+++ src/main/java/study/jpql/JpqlMain.java
@@ -35,7 +35,7 @@ public class JpqlMain {
       em.flush();
       em.clear();
 
-      String query = "select locate('de', 'abcdefg') from Member m";
+      String query = "select size(t.members) from Team t";
       List<Integer> resultList = em.createQuery(query, Integer.class).getResultList();
       for (Integer integer : resultList) {
         System.out.println("integer = " + integer);
---
+++ src/main/java/study/jpql/domain/MyH2Dialect.java
@@ -0,0 +1,9 @@
+//package study.jpql.domain;
+//
+//import org.hibernate.dialect.H2Dialect;
+//
+//public class MyH2Dialect extends H2Dialect {
+//  public MyH2Dialect() {
+//    registerFunction();
+//  }
+//}
