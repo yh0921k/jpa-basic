d99aee38c1d130cf2e65f9712d0dc06f3413a378
Yonghwi Kim <yh0921k@gmail.com>
2022-11-01 21:00:00 +0900

    feat: Section 11. FETCH JOIN 기본

--- src/main/java/study/jpql/JpqlMain.java
+++ src/main/java/study/jpql/JpqlMain.java
@@ -21,32 +21,52 @@ public class JpqlMain {
     tx.begin();
 
     try {
-      Team team = new Team();
-      team.setName("team");
-      em.persist(team);
+      Team teamA = new Team();
+      teamA.setName("TeamA");
+      em.persist(teamA);
+
+      Team teamB = new Team();
+      teamB.setName("teamB");
+      em.persist(teamB);
+
+      Team teamC = new Team();
+      teamC.setName("teamC");
+      em.persist(teamC);
 
       Member member1 = new Member();
-      member1.setUsername("관리자1");
+      member1.setUsername("회원1");
       member1.setAge(20);
-      member1.setTeam(team);
+      member1.setTeam(teamA);
       member1.setType(MemberType.ADMIN);
       em.persist(member1);
 
       Member member2 = new Member();
-      member2.setUsername("관리자2");
+      member2.setUsername("회원2");
       member2.setAge(20);
-      member2.setTeam(team);
+      member2.setTeam(teamA);
       member2.setType(MemberType.ADMIN);
       em.persist(member2);
+
+      Member member3 = new Member();
+      member3.setUsername("회원3");
+      member3.setAge(20);
+      member3.setTeam(teamB);
+      member3.setType(MemberType.ADMIN);
+      em.persist(member3);
+
+      Member member4 = new Member();
+      member4.setUsername("회원2");
+      member4.setAge(20);
+      member4.setType(MemberType.ADMIN);
+      em.persist(member4);
 
       em.flush();
       em.clear();
 
-      String query = "select m.username from Team t join t.members m";
-//      Collection result = em.createQuery(query, Collection.class).getResultList();
-      List<String> resultList = em.createQuery(query, String.class).getResultList();
-      for (Object o : resultList) {
-        System.out.println("o = " + o);
+      String query = "select m from Member m join fetch m.team";
+      List<Member> resultList = em.createQuery(query, Member.class).getResultList();
+      for (Member member : resultList) {
+        System.out.println("member = " + member);
       }
 
       tx.commit();
